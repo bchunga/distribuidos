065218598f823d56b02387c519051fafa13af6af
bchunga <bryan.chunga.easymetering@gmail.com>
2022-08-03 18:11:34 -0500

    first commit

---
+++ src/test/java/com/example/g1project/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g1project.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
